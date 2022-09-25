3a8f3f9098828841f771ad1bc63efab10eaa9e4a
one6n <clod94@live.it>
2022-09-25 12:20:59 +0200

    first commit

---
+++ src/main/java/it/one6n/report/integrator/ReportIntegratorApplication.java
@@ -0,0 +1,13 @@
+package it.one6n.report.integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/one6n/report/integrator/ReportIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.one6n.report.integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
