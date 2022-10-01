bed85e301c2bb07f466bce758d4691b9fccf3d60
one6n <clod94@live.it>
2022-10-01 21:45:16 +0200

    added swagger, minor refactor on api controller

--- src/main/java/it/one6n/report/integrator/controllers/ApiRestController.java
+++ src/main/java/it/one6n/report/integrator/controllers/ApiRestController.java
@@ -1,7 +1,9 @@
 package it.one6n.report.integrator.controllers;
 
+import java.security.InvalidParameterException;
 import java.util.NoSuchElementException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.swagger.v3.oas.annotations.Operation;
 import it.one6n.report.integrator.models.ReportConfiguration;
 import it.one6n.report.integrator.records.TypedRestResult;
 import it.one6n.report.integrator.records.dto.ReportConfigurationDto;
@@ -41,19 +44,26 @@ public class ApiRestController {
 		this.reportConfigurationService = reportConfigurationService;
 	}
 
+	@Operation(summary = "Get a Report Configuration by Customer")
 	@GetMapping(path = ApiRestController.GET_CONFIGURATION_PATH, produces = "application/json")
 	public ResponseEntity<TypedRestResult<ReportConfigurationDto>> getReportConfiguration(
 			@PathVariable String customer) {
 		log.info("method=getReportConfiguration, customer={}", customer);
 		ResponseEntity<TypedRestResult<ReportConfigurationDto>> responseEntity = null;
 		try {
+			if (StringUtils.isBlank(customer))
+				throw new InvalidParameterException("The customer is null or empty");
 			ReportConfiguration reportConfiguration = getReportConfigurationService().findByCustomer(customer);
 			responseEntity = ResponseEntity.status(HttpStatus.OK).body(new TypedRestResult<ReportConfigurationDto>(true,
 					null, ReportConfigurationMapperUtils.entityToDto(reportConfiguration)));
+		} catch (InvalidParameterException e) {
+			log.error(e.getMessage(), e);
+			responseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new TypedRestResult<ReportConfigurationDto>(false, e.getMessage(), null));
 		} catch (NoSuchElementException e) {
 			log.error(e.getMessage(), e);
 			responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND)
-					.body(new TypedRestResult<ReportConfigurationDto>(false, e.getMessage(), null));
+					.body(new TypedRestResult<ReportConfigurationDto>(false, "No report configuration found", null));
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			responseEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -62,16 +72,24 @@ public class ApiRestController {
 		return responseEntity;
 	}
 
+	@Operation(summary = "Create new Report Configuration")
 	@PostMapping(path = ApiRestController.CREATE_CONFIGURATION_PATH, consumes = "application/json", produces = "application/json")
 	public ResponseEntity<TypedRestResult<ReportConfigurationDto>> createReportConfiguration(
 			@RequestBody ReportConfigurationDto reportConfigurationDto) {
 		log.info("method=createReportConfiguration, reportConfigurationDto={}", reportConfigurationDto);
 		ResponseEntity<TypedRestResult<ReportConfigurationDto>> responseEntity = null;
 		try {
+			if (reportConfigurationDto == null || StringUtils.isBlank(reportConfigurationDto.customer())
+					|| StringUtils.isNotBlank(reportConfigurationDto.id()))
+				throw new InvalidParameterException("The configuration is not valid");
 			ReportConfiguration reportConfiguration = getReportConfigurationService()
 					.saveReportConfiguration(ReportConfigurationMapperUtils.dtoToEntity(reportConfigurationDto));
 			responseEntity = ResponseEntity.status(HttpStatus.CREATED).body(new TypedRestResult<ReportConfigurationDto>(
 					true, null, ReportConfigurationMapperUtils.entityToDto(reportConfiguration)));
+		} catch (InvalidParameterException e) {
+			log.error(e.getMessage(), e);
+			responseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new TypedRestResult<ReportConfigurationDto>(false, e.getMessage(), null));
 		} catch (DuplicateKeyException e) {
 			log.error(e.getMessage(), e);
 			responseEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
